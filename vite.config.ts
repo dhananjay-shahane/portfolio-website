fa4a51a19f0680db2d9ac9369efa59d0f0c821fa
Dhananjay Shahane <143813959+DhananjayShahane@users.noreply.github.com>
2025-05-24 09:22:50 +0530

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -14,15 +14,14 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+      "@": path.resolve(__dirname, "src"),
+      "@shared": path.resolve(__dirname, "../shared"),
+      "@assets": path.resolve(__dirname, "../attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"),
   base: "./",
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: "dist",
     emptyOutDir: true,
   },
   server: {
